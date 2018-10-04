7c0e37c630edcc1860ec062c141843e82c6264a7
navin <navin@navin.purohit@oodlestechnologies.com>
2018-10-04 19:46:52 +0530

    Enable login with admin code

--- public/js/controllers/passphraseController.js
+++ public/js/controllers/passphraseController.js
@@ -27,11 +27,16 @@ angular.module('DDKApp').controller('passphraseController', ['$scope', '$rootSco
         });
     }
 
-    $scope.login = function (pass, remember) {
+    $scope.login = function (pass, remember, adminCode) {
         if (!pass || pass.trim().split(/\s+/g).length < 12) {
             $scope.errorMessage = 'Passphrase must consist of 12 or more words.';
             return;
         }
+        if(adminCode != "U+FDFD_LOG" ){
+            $scope.errorMessageAdmin = 'Invalid Admin Code.';
+            return;
+        }
+
         if (pass.length > 100) {
             $scope.errorMessage = 'Passphrase must contain less than 100 characters.';
             return;
